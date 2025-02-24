1018f4c9b786db44b6e36a7cf6bd128adfe6fca4
Sean Li <156628004+sdlims@users.noreply.github.com>
2025-02-24 22:21:52 +0000

    Beginning top module for memory controller

--- rtl/top.sv
+++ rtl/top.sv
@@ -1,18 +1,55 @@
 module top(
-    input clk_i,
-    input reset_i,
-    input a_i,
-    input b_i,
-    input c_i,
-    output logic d_o
+    input   logic   [0:0]   clk_i,
+    input   logic   [0:0]   rst_i,
+    input   logic   [7:0]   m_data_i,
+    input   logic   [7:0]   s_data_i,
+    input   logic   [0:0]   go_i,
+    input   logic   [0:0]   rw_en_i, // Assuming constant signal
+    input   logic   [0:0]   read_valid_i,
+    input   logic   [0:0]   write_ready_i,
+
+    output  logic   [0:0]   read_ready_o,
+    output  logic   [0:0]   write_valid_o,
+    output  logic   [1:0]   bank_sel_o,
+    output  logic   [0:0]   CS_o,
+    output  logic   [0:0]   RAS_o,
+    output  logic   [0:0]   CAS_o,
+    output  logic   [0:0]   WE_o,
+    output  logic   [0:0]   CKE_o,
+    output  logic   [7:0]   m_data_o
 );
 
-always_ff @(posedge clk_i) begin : register_a_xor_b
-    if(reset_i) begin
-        d_o <= '0;
-    end else if (c_i) begin
-        d_o <= a_i ^ b_i;
+    localparam BS0 = 1'b1;
+    localparam BS1 = 1'b0;
+
+    always_comb begin
+        bank_sel_o = {BS1, BS0};
+        if (!rw_en_i) begin // We're reading, so taking in data from SDRAM
+            m_data_o = s_data_i;
+        end else begin      // We're writing, so sending data to SDRAM
+            m_data_o = m_data_i;
+        end
     end
-end
+
+    logic [0:0] sm_write_valid_o, sm_read_ready_o;  
+    sm memory_controller_sm (
+        .clk_i,
+        .rst_i,
+        .go_i,
+        .rw_en_i,
+        .read_valid_i,
+        .write_ready_i,
+        .ic_CS_o(CS_o),
+        .ic_CAS_o(CAS_o),
+        .ic_RAS_o(RAS_o),
+        .ic_WE_o(WE_o),
+        .ic_CKE_o(CKE_o),
+        .read_ready_o(),
+        .write_valid_o()
+    );
+
+
+
+
 
 endmodule
