9d31e3b5e2884e5d78889eac4623fde1222879d4
Sean Li <156628004+sdlims@users.noreply.github.com>
2025-03-03 04:07:43 +0000

    Sorter Testbench Tools

---
+++ dv/sorter_runner.sv
@@ -0,0 +1,64 @@
+`timescale 1ns/1ps
+
+module sorter_runner;
+
+parameter realtime ClockPeriod = 20ns;
+
+logic [0:0] clk_i;
+logic [0:0] rst_i;
+logic [0:0] start_i;
+
+initial begin
+    clk_i = 0;
+    forever begin
+        #(ClockPeriod/2);
+        clk_i = !clk_i;
+    end
+end
+
+localparam DATA_E = 8;
+localparam DATA_W = 8;
+
+logic [DATA_W - 1:0] dut_data_i, dut_data_o;
+sorter #(
+    .DATA_ENTRIES(DATA_E),
+    .DATA_WIDTH(DATA_W)
+) DUT (
+    .clk_i,
+    .rst_i,
+    .write_valid_i,
+    .start_i,
+    .data_i(dut_data_i),
+    .receive_ready_o(),
+    .read_valid_o(),
+    .data_o(dut_data_o)
+);
+
+task automatic reset_to_start;
+    rst_i <= 1'b1;
+    start_i <= 1'b0;
+    @(posedge clk_i);
+    rst_i <= 1'b0;
+    start_i <= 1'b1;
+    @(posedge clk_i);
+    start_i <= 1'b0;
+endtask
+
+
+task automatic delay;
+    int cycles = $urandom_range(10, 20);
+    for (int i = 0; i < cycles; i++) begin
+        @(posedge clk_i); #1;
+    end
+endtask
+
+logic [0:0] write_valid_i;
+task automatic ready_and_write;
+    write_valid_i <= 1'b1;
+    dut_data_i <= {DATA_W{1'b1}};
+    @(posedge clk_i);
+    dut_data_i <= {DATA_W{1'b0}};
+    write_valid_i <= 1'b0;
+endtask
+
+endmodule
---
+++ dv/sorter_tb.sv
@@ -0,0 +1,24 @@
+module sorter_tb;
+
+sorter_runner sorter_runner();
+
+initial begin
+    $dumpfile( "dump.fst" );
+    $dumpvars;
+    $display( "Begin simulation." );
+    $urandom(100);
+    $timeformat( -3, 3, "ms", 0);
+
+    sorter_runner.reset_to_start();
+    repeat (8) begin
+        // Delay some random time
+        sorter_runner.ready_and_write();
+        sorter_runner.delay();
+    end
+
+    #1000;
+    $display( "End simulation." );
+    $finish;
+end
+
+endmodule
