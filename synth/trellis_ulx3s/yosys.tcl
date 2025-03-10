510770f01181e0797d1978407f3eb92cfeaa17d1
Sean Li <156628004+sdlims@users.noreply.github.com>
2025-03-10 21:03:38 +0000

    modified paths for yosys.tcl and top

--- synth/trellis_ulx3s/yosys.tcl
+++ synth/trellis_ulx3s/yosys.tcl
@@ -3,7 +3,8 @@ yosys -import
 
 read_verilog synth/build/rtl.sv2v.v synth/trellis_ulx3s/ulx3s.v
 
-synth_ecp5 -top ulx3s
+# synth_ecp5 -top ulx3s
+synth_ecp5 -top sorter
 
-write_verilog -noexpr -noattr -simple-lhs synth/trellis_ulx3s/build/synth.v
-write_json synth/trellis_ulx3s/build/synth.json
+write_verilog -noexpr -noattr -simple-lhs synth/build/synth.v
+write_json synth/build/synth.json
