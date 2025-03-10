4dea860c48c3b30d22f57ac7b476d16aa81cb15c
Sean Li <156628004+sdlims@users.noreply.github.com>
2025-03-10 21:01:58 +0000

    Modified gls.f to accomodate new runner and synth build dir

--- synth/trellis_ulx3s/gls.f
+++ synth/trellis_ulx3s/gls.f
@@ -6,6 +6,9 @@ ${YOSYS_DATDIR}/ecp5/cells_sim.v
 ${YOSYS_DATDIR}/ecp5/cells_bb.v
 
 -I${YOSYS_DATDIR}/ecp5
+-I${TMATMUL_DIR}/pipelined_mem.sv
 
-synth/trellis_ulx3s/build/synth.v
-synth/trellis_ulx3s/ulx3s_runner.sv
+// synth/trellis_ulx3s/build/synth.v
+synth/build/synth.v
+synth/trellis_ulx3s/sorter_runner.sv
+
