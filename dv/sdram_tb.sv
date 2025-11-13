6e4791b6c0d21535f55a1f6e35da88e4de1496c1
Sean Li <theseanisalie@gmail.com>
2025-11-13 14:48:17 -0800

    Updated tb for WRITE actions

--- dv/sdram_tb.sv
+++ dv/sdram_tb.sv
@@ -23,14 +23,14 @@ logic [12:0] addr_o;
 
 wire [15:0] data_io;
 
-//logic [15:0] data_io_l;
-
+logic [15:0] data_io_l;
 assign data_io = data_io_l;
 
 
 
 //clock period is gonna be 7.5188 for 133mhz clock
-parameter realtime ClockPeriod = 7.5188ns; //i think this is allowed, but i need to double check
+// 6.06 for 165MHz
+parameter realtime ClockPeriod = 6.06ns; //i think this is allowed, but i need to double check
 initial begin
     clk_i = 0;
     forever begin
@@ -43,74 +43,95 @@ end
 top 
 #()
 dut(
-    .clk_i       (clk_i),
-    .rst_i       (reset_i),
-    .addr_i      (addr_i),
-    .m_data_i    (m_data_i),
-    .go_i        (go_i),
-    .rw_en_i     (rw_en_i),
-    .read_valid_i(read_valid_i),
-    .write_ready_i(write_ready_i),
-    .read_ready_o(read_ready_o),
-    .write_valid_o(write_valid_o ),
-    .bank_sel_o  (bank_sel_o),
-    .CS_o        (CS_o),
-    .RAS_o       (RAS_o),
-    .CAS_o       (CAS_o),
-    .WE_o        (WE_o),
-    .CKE_o       (CKE_o),
-    .m_data_o    (m_data_o),
-    .addr_o      (addr_o),
-    .data_io     (data_io)
+    .clk_i          (clk_i),
+    .rst_i          (reset_i),
+    .addr_i         (addr_i),
+    .m_data_i       (m_data_i),
+    .go_i           (go_i),
+    .rw_en_i        (rw_en_i),
+    .read_valid_i   (read_valid_i),
+    .write_ready_i  (write_ready_i),
+    .read_ready_o   (read_ready_o),
+    .write_valid_o  (write_valid_o),
+    .bank_sel_o     (bank_sel_o),
+    .CS_o           (CS_o),
+    .RAS_o          (RAS_o),
+    .CAS_o          (CAS_o),
+    .WE_o           (WE_o),
+    .CKE_o          (CKE_o),
+    .m_data_o       (m_data_o),
+    .addr_o         (addr_o),
+    .data_io        (data_io)
 );
 
-// W9825G6KH 
-// dut (
-//     .Dq    (data_io),
-//     .Addr  (addr_o),
-//     .Bs    (bank_sel_o),
-//     .Clk   (clk_i),
-//     .Cke   (CKE_o),
-//     .Cs_n  (CS_o),
-//     .Ras_n (RAS_o),
-//     .Cas_n (CAS_o),
-//     .We_n  (WE_o),
-//     .Dqm   ('b0) 
-// );
+W9825G6KH uut (
+    .Dq    (data_io),
+    .Addr  (addr_o),
+    .Bs    (bank_sel_o),
+    .Clk   (clk_i),
+    .Cke   (CKE_o),
+    .Cs_n  (CS_o),
+    .Ras_n (RAS_o),
+    .Cas_n (CAS_o),
+    .We_n  (WE_o),
+    .Dqm   ('b0) 
+);
+
+task t_initial();
+    begin
+        reset_i = 1'b0;
+        addr_i = '0;
+        m_data_i = '0;
+        go_i = 1'b0;
+        read_valid_i = 1'b0;
+        write_ready_i = 1'b0;
+        data_io_l = 'x;
+        rw_en_i = 1'b0; // READ
+
+        @(negedge clk_i);
+
+        reset_i = 1'b1;
+        repeat(3) @(negedge clk_i);
+        reset_i = 1'b0;
+
+        @(negedge clk_i);
+        go_i = 1'b1;
+
+        @(negedge clk_i);
+        go_i = 1'b0;
+
+        #1000; // Enables Time for PC and BA
+    end
+endtask
+
+task t_write();
+    begin
+
+        @(negedge clk_i);
+        rw_en_i = 1'b1;
+        write_ready_i = 1'b1;
+        data_io_l = 'd10;
+
+        @(negedge clk_i);
+        data_io_l = 'd11;
+
+        @(negedge clk_i);
+        data_io_l = 'd22;
+
+        @(negedge write_valid_o);
+        write_ready_i = 1'b0;
+
+    end
+endtask
 
 initial begin
-$dumpfile("sdram.vcd");
-$dumpvars();
-//#100
-reset_i = 1'b1;
-@(posedge clk_i);
-reset_i = 1'b0;
-#1
-//bank select already handled
-addr_i = 13'b0;
-m_data_i = 16'b1;
-go_i = 1'b1;
 
-//for read
-rw_en_i = 1'b1;
-write_ready_i = 1'b1;
+    t_initial();
+    @(negedge clk_i);
 
+    t_write();
 
-//ill change this to be more precise later
-#1000
-//for write
-write_ready_i = 1'b0;
-rw_en_i = 1'b0;
-read_valid_i = 1'b1;
-
-#100
-go_i = 0;
-#1000
-
-
-
-$display("test");
-$finish();
+    $finish();
 end
 
 endmodule
